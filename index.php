84906b19841007fa3f35f4f24d4cc1691733d31e
dstaff <danis.staff@gmail.com>
2023-07-18 16:56:47 +0000

    removing debuggers

--- index.php
+++ index.php
@@ -1,28 +1,21 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-echo "x1";
+
 require_once 'dbconfig.php';
-echo "x2";
+
 $f3->route('GET /', 'getQuestions');
-echo "x3";
+
 function getQuestions()
 {
     $pdo = $GLOBALS['pdo'];
-    //$query = "SELECT MIN(id) AS minimo,MAX(id) AS maximo FROM questions";
-    echo "x4";
-    $query = "SELECT * FROM questions";
-    echo "x5";
+    $query = "SELECT MIN(id) AS minimo,MAX(id) AS maximo FROM questions";
     // Prepare and execute the query
     $stmt = $pdo->prepare($query);
-    echo "x6";
     $stmt->execute();
-    echo "x7";
     // Fetch all books from the result set
     $questions = $stmt->fetchAll();
-    echo "x8";
     // Return the books as JSON response
     echo json_encode($questions);
-    echo "x9";
 }
 
 $f3->run();
