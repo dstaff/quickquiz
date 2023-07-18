9c4e562a441ce55ed23c409ff9c967653452489e
dstaff <danis.staff@gmail.com>
2023-07-18 18:58:42 +0000

    rewrite the corse

--- dbconfig.php
+++ dbconfig.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
 require '../f3/vendor/autoload.php';
 
 $config = parse_ini_file('config.ini', true);
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-
 require_once 'dbconfig.php';
 
 $f3->route('GET /', 'getQuestions');
