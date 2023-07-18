bf3dd8daf99dc33397343143201fd1603cd92813
dstaff <danis.staff@gmail.com>
2023-07-18 19:06:53 +0000

    new allow methods

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: *");
+//header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 require '../f3/vendor/autoload.php';
 
